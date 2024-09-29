9da5be1e8adbbe915ac7fc42d820cad3cb6065f0
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-09-29 19:21:44 +0300

    Initial settings

---
+++ app/sources/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  SimpleApp
+//
+//  Created by Stanislav Lemeshaev on 29.09.2024.
+//  Copyright © 2024 iOS Diving. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
